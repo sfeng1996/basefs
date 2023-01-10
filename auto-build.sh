dd644371dec62bca8b1c5f392cb608d6a9150630
sfeng <SFeng1996@163.com>
2023-01-10 14:33:11 +0800

    Update auto-build.sh

--- auto-build.sh
+++ auto-build.sh
@@ -105,6 +105,6 @@ if [[ "$push" == "true" ]]; then
   if [[ -n "$username" ]] && [[ -n "$password" ]]; then
     sudo ./sealer login docker.io -u sfeng1996 -p sfeng19960921
   fi
-  export SKIP_TLS_VERIFY=true
+  export SKIP_TLS_VERIFY=false
   sudo ./sealer push "${buildName}"
 fi
