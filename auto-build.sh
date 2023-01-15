5c7d1c6ea938f340c6da2bec8bff48a9216948f7
sfeng <SFeng1996@163.com>
2023-01-15 14:29:17 +0800

    Update auto-build.sh

--- auto-build.sh
+++ auto-build.sh
@@ -87,7 +87,7 @@ sudo chmod +x version.sh download.sh && export kube_install_version="$k8s_versio
 ./download.sh "${cri}"
 
 sudo chmod +x amd64/bin/kube* && sudo chmod +x arm64/bin/kube*
-sudo wget "https://github.com/sealerio/sealer/releases/download/v0.8.7/sealer-v0.8.7-linux-${ARCH}.tar.gz" && sudo tar -xvf "sealer-v0.8.7-linux-${ARCH}.tar.gz"
+sudo wget "https://github.com/sealerio/sealer/releases/download/v0.9.0/sealer-v0.9.0-linux-${ARCH}.tar.gz" && sudo tar -xvf "sealer-v0.9.0-linux-${ARCH}.tar.gz"
 sudo sed -i "s/v1.19.8/$k8s_version/g" rootfs/etc/kubeadm.yml ##change k8s_version
 if [[ "$cri" == "containerd" ]]; then sudo sed -i "s/\/var\/run\/dockershim.sock/\/run\/containerd\/containerd.sock/g" rootfs/etc/kubeadm.yml; fi
 sudo sed -i "s/kubeadm.k8s.io\/v1beta2/$kubeadmApiVersion/g" rootfs/etc/kubeadm.yml
